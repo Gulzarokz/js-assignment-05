086cfd8e51dc2ac6ecdca3d876633b66c5110b4f
Gulzarokz <gokz22301@gamil.com>
2024-10-15 21:45:13 +0500

    add if if esle with exercise

---
+++ index.js
@@ -0,0 +1,131 @@
+// chapter NO 11
+
+// console.log(1 == 1); // true
+
+// console.log(1 == "1"); it not check data types
+
+// console.log(1 == 2);
+ 
+// now check === equal
+
+// console.log(1 === 1); 
+
+// console.log(1 === "1");
+
+// console.log(1 === 2);
+
+// (!)
+
+// console.log(!true);
+// console.log(!false);
+
+// not equal (!==, !=)
+
+// console.log(1 != 1);
+// console.log(1 != "1");
+// console.log(1 != 2);
+
+// not equal (!==)
+// console.log(1 !== 1);
+// console.log(1 !== "1");
+// console.log(1 !== 2);
+
+// greater than or less than opertor > or <
+// console.log(1 > 2);
+// console.log(2 > 1);
+// console.log(1 > 1);
+// console.log(1 > "1");
+// console.log(1 > 0);
+
+// less than <
+// console.log(1 < 2);
+// console.log(2 < 1);
+// console.log(1 < 1);
+// console.log(1 < "1");
+// console.log(1 < 0);
+
+// console.log(1 >= 2);
+// console.log(2 >= 1);
+// console.log(1 >= 1);
+// console.log(1 >= "1");
+// console.log(1 >= 0);
+
+// // // less than <
+// console.log(1 <= 2);
+// console.log(2 <= 1);
+// console.log(1 <= 1);
+// console.log(1 <= "1");
+// console.log(1 <= 0);
+
+
+// if_else statements
+
+// const badal = true;
+// const badal = false;
+
+// if(badal){
+//     alert("it rain today in peshawar");
+// }
+// if(!badal){
+//     alert("no chance of rain ");
+// }
+
+//  const badal = true;
+// if(!badal){
+//     alert("it rain today in peshawar soon");
+// }
+// else{
+//     alert("No chance of rain")
+// }
+
+// const bhook = true;
+// const bhook = false;
+// const paise = true;
+// if(bhook && paise){
+//     alert("Get the buger");
+// }
+// else{
+//     alert("No bhook no burger")
+// }
+//  negate karna
+// const badal = !false;
+// console.log(badal);
+//  const badall = false;
+//  console.log(!badall);
+ 
+
+// odd & Even number 
+
+// const userName = prompt("please Enter Name first ");
+// const userInput = +prompt("Enter a number please");
+// reminder = userInput % 2;
+//  if(reminder === 0){
+//     alert("You Enter a even number");
+
+//  }
+//  else{
+//     alert("you enter a an odd number");
+//  }
+
+//  var x = prompt("Where does the Pope live?");
+//  if (x === "Vatican") {
+//  alert("Correct!");
+//  }
+
+// var x = prompt("Where does the Pope live?");
+//  if (x === "Vatican") {
+//  alert("Correct!");
+//  }
+//  if (x !== "Vatican") {
+//  alert("Wrong answer");
+//  }
+
+// var correctAnswer = "Vatican";
+//  if (x === correctAnswer) {
+//  alert("Correct!");
+//  }
+//  else {
+//  score--;
+//  userIQ = "problematic";
+//  alert("Incorrect");
+//  }
